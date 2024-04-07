6004f5674d5ea8105ff575b916b300e801121cce
André Felipe Siqueira <andrefs.995@gmail.com>
2024-04-07 15:55:57 +0100

    Build test + Reimport Z3

---
+++ Assets/Plugins/Z3/Utils/ExtensionMethods/ExtensionMethods/UnityExtensions.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using Object = UnityEngine.Object;
+
+namespace Z3.Utils.ExtensionMethods
+{
+    /// <summary>
+    /// General Extensions
+    /// </summary>
+    public static class UnityExtensions
+    {
+        public static T CloneT<T>(this T node) where T : Object
+        {
+            return Object.Instantiate(node);
+        }
+
+        public static bool ObjectNullCheck(this object target)
+        {
+            return target == null || (target is Object obj && !obj);
+        }
+
+        public static void DoActionNextFrame(this MonoBehaviour monoBehaviour, Action action) // Prevents Event system bugs
+        {
+            monoBehaviour.StartCoroutine(CallNextFrame(action));
+        }
+
+        /// <summary> Prevents Event system bugs </summary>
+        public static void SelectWithDelay(this Selectable selectable)
+        {
+            selectable.StartCoroutine(CallNextFrame(selectable.Select));
+        }
+
+        /// <summary> Prevents Event system bugs </summary>
+        public static void SelectWithDelay(this MonoBehaviour monoBehaviour, GameObject gameObject)
+        {
+            monoBehaviour.StartCoroutine(CallNextFrame(() => EventSystem.current.SetSelectedGameObject(gameObject)));
+        }
+
+        private static IEnumerator CallNextFrame(Action action)
+        {
+            yield return new WaitForEndOfFrame();
+            action();
+        }
+
+        public static int ToIntLayer(this LayerMask layer) => (int)Mathf.Log(layer.value, 2f);
+    }
+}
--- Assets/Scripts/Shared/ServiceLocator/ServiceLocator.cs
+++ Assets/Scripts/Shared/ServiceLocator/ServiceLocator.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Reflection;
+using Z3.Utils;
 
 namespace Marmalade.TheGameOfLife.Shared
 {
